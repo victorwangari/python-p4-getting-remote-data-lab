14f6cce6795c49d0562643f0e939e02a172ef4a0
Victor Wangari <victorwangari65187@gmail.com>
2024-06-29 12:11:05 +0300

    initial commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
+
         pass
 
     def load_json(self):
+        response = self.get_response_body()
+        return json.loads(response)
+
         pass
